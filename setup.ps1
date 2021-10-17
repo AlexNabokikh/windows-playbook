21b19daf2146dc222b6bdec49ab6d67b9d52c3c5
Alexander Nabokikh <nabokih.a@gmail.com>
2021-10-17 13:22:57 +0200

    Fixed setup.ps script

--- setup.ps1
+++ setup.ps1
@@ -1,37 +1,37 @@
-# Create helper functuion
-function Test-Command($cmdname) {
-    return [bool](Get-Command -Name $cmdname -ErrorAction SilentlyContinue)
-}
-
 # Ensure chocolatey installed
-if (Test-Command -cmdname 'choco') {
-    Write-Host "Chocolatey is already installed, skip installation." -ForegroundColor Green
+if ([bool](Get-Command -Name 'choco' -ErrorAction SilentlyContinue)) {
+    Write-Verbose "Chocolatey is already installed, skip installation." -Verbose
 }
 else {
-    Write-Host "Installing Chocolatey first..." -ForegroundColor Yellow
+    Write-Verbose "Installing Chocolatey..." -Verbose
     Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-    Write-Host "Chocolatey is installed" -ForegroundColor Green
 }
 
-# Get a list of OpenSSH packages
-$openSSHpackages = Get-WindowsCapability -Online | Where-Object Name -like 'OpenSSH*'
-
-# Install the OpenSSH Server and Client
-foreach ($package in $openSSHpackages) {
-    Add-WindowsCapability -Online -Name $package
-}
-
-# Start the sshd service
-Start-Service sshd
-
-# OPTIONAL but recommended:
-Set-Service -Name sshd -StartupType 'Automatic'
-
-# Confirm the Firewall rule is configured. It should be created automatically by setup. Run the following to verify
-if (!(Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
-    Write-Output "Firewall Rule 'OpenSSH-Server-In-TCP' does not exist, creating it..."
-    New-NetFirewallRule -Name 'OpenSSH-Server-In-TCP' -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
+# Ensure OpenSSH Server installed
+if ([bool](Get-Service -Name sshd)) {
+    Write-Verbose "OpenSSH is already installed, skip installation." -Verbose
 }
 else {
-    Write-Output "Firewall rule 'OpenSSH-Server-In-TCP' has been created and exists."
+    Write-Verbose "Installing OpenSSH..." -Verbose
+    $openSSHpackages = Get-WindowsCapability -Online | Where-Object Name -like 'OpenSSH*'
+
+    foreach ($package in $openSSHpackages) {
+        Add-WindowsCapability -Online -Name $package
+    }
+
+    # Start the sshd service
+    Write-Verbose "Starting OpenSSH service..." -Verbose
+    Start-Service sshd
+    Set-Service -Name sshd -StartupType 'Automatic'
+
+    # Confirm the Firewall rule is configured. It should be created automatically by setup. Run the following to verify
+    Write-Verbose "Confirm the Firewall rule is configured..." -Verbose
+    if (!(Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
+        Write-Output "Firewall Rule 'OpenSSH-Server-In-TCP' does not exist, creating it..."
+        New-NetFirewallRule -Name 'OpenSSH-Server-In-TCP' -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
+    }
+    else {
+        Write-Output "Firewall rule 'OpenSSH-Server-In-TCP' has been created and exists."
+    }
 }
+
